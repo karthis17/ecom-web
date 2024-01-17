ce0ae9c3878c748c9b9b169279cecbc2d77ba05b
karthis17 <karthirs602@gmail.com>
2024-01-17 11:07:37 +0530

    page number was added

--- src/app/product-list-component/product-list-component.component.ts
+++ src/app/product-list-component/product-list-component.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnChanges, SimpleChanges } from '@angular/core';
 import { Product } from '../models/product.model';
 import { CurrencyPipe, NgFor, NgIf } from '@angular/common';
 import { ProdectService } from '../service/prodect.service';
@@ -14,17 +14,40 @@ import { Router } from '@angular/router';
   styleUrl: './product-list-component.component.css'
 })
 export class ProductListComponentComponent {
-  @Input('category') category: string = 'PC & Laptops';
-  @Input('product') product: Product[] = [];
+  @Input('category') category: string | null = null;
+  @Input('product') product!: Product[];
   @Input('isHome') isHome: boolean = true;
 
   @Output('clickedProd') clickedProd: EventEmitter<any> = new EventEmitter();
   constructor(private prodect: ProdectService, private router: Router) { }
 
-  showProduct: number = 10;
+  start!: number;
+  end!: number;
+
+  pageNumbers!: number[];
+
+  activePage: number = 1;
 
   ngOnInit() {
-    this.prodect.fectDataCategoryWis(this.category).subscribe(data => this.product = data);
+
+    console.log(this.product, "hi")
+    if (this.category) {
+      this.prodect.fectDataCategoryWis(this.category).subscribe(data => {
+        this.product = data;
+        this.start = 0;
+        this.end = 10;
+      });
+    }
+
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['product'] && !this.category) {
+      this.start = 0;
+      this.end = 10;
+      this.pageNumbers = Array.from({ length: Math.ceil(this.product.length / 10) });
+    }
+
   }
 
   nav(id: any) {
@@ -32,14 +55,14 @@ export class ProductListComponentComponent {
     this.isHome ? this.router.navigate(['/', 'product', id]) : this.clickedProd.emit(id);
   }
 
-  showMore() {
-    if (this.showProduct < this.product.length)
-      this.showProduct = this.showProduct * 10
+  changePage(page_number: number) {
+    this.start = (page_number - 1) * 10;
+    this.end = page_number * 10;
+
+    this.activePage = page_number;
+
+    console.log(this.start, this.end)
   }
 
-  showLess() {
-    if (this.showProduct > 10)
-      this.showProduct = this.showProduct / 10;
-  }
 
 }
